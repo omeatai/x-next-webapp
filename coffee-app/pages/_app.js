205dbdd16227c77de5ebab591b2726789c2dcc5b
omeatai <omeatai@gmail.com>
2023-10-21 21:12:33 -0600

    3. Next app Layout
    3. Next app Layout

--- coffee-app/pages/_app.js
+++ coffee-app/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>&copy; 2023 Coffee App</p>
+      </footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
