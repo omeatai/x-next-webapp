cd4814ea22b697d859cd515cd5a9e16b4b320fc2
omeatai <omeatai@gmail.com>
2023-10-22 10:53:59 -0600

    9. Add routes using Next Link
    9. Add routes using Next Link

--- coffee-app/pages/coffee-store/[id].js
+++ coffee-app/pages/coffee-store/[id].js
@@ -1,21 +1,20 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStoreDetail = () => {
   const router = useRouter();
 
   console.log("router", router);
-  console.log("router-path", router.asPath);
-  console.log("router-path-arr", router.asPath.split("/"));
   console.log("router-query", router.query);
-
-  const routearr = router.asPath.split("/");
-  const routenum = routearr[routearr.length - 1];
 
   const queryId = router.query.id;
 
   return (
     <div>
-      <h1>This is a dynamic route for CoffeeStoreDetail {routenum}.</h1>
+      <Link href={"/coffee-store"} as={"Home"} passHref={true} scroll={true}>
+        <a>Back to Home</a>
+      </Link>
+      <h1>This is a dynamic route for CoffeeStoreDetail {queryId}.</h1>
       <h2>The Query ID is {queryId}.</h2>
     </div>
   );
--- coffee-app/pages/coffee-store/index.js
+++ coffee-app/pages/coffee-store/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 const CoffeeStore = () => {
   return (
     <div>
       <h1>The Coffee Store Page</h1>
+      <ul>
+        <li>
+          <Link href={"/coffee-store/1"}>
+            <a>Store 1</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={"/coffee-store/2"}>
+            <a>Store 2</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={"/coffee-store/3"}>
+            <a>Store 3</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
