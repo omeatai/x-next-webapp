cab6c3af3ce7996a2d79c5584d8870e93d7865b8
omeatai <omeatai@gmail.com>
2023-10-23 18:24:24 -0600

    12. Next Hero Image Component
    12. Next Hero Image Component

--- coffee-app/pages/index.js
+++ coffee-app/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/Banner";
@@ -21,6 +22,9 @@ export default function Home() {
           buttonText="View stores nearby..."
           handleOnClick={handleOnBannerBtnClick}
         />
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.svg" width={700} height={400} />
+        </div>
       </main>
     </div>
   );
