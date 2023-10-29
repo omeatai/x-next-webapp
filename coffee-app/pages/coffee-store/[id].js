ea4f1c64e2d2300457d7a770ade4eb8e73dd32a1
omeatai <omeatai@gmail.com>
2023-10-29 09:44:08 -0600

    22. Coffee Store App - getStaticPaths
    22. Coffee Store App - getStaticPaths

--- coffee-app/pages/coffee-store/[id].js
+++ coffee-app/pages/coffee-store/[id].js
@@ -1,12 +1,34 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-const CoffeeStoreDetail = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export async function getStaticProps({ params }) {
+  console.log("params", params);
+
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        console.log("coffeeStoreId", coffeeStore.id);
+        return coffeeStore.id === parseInt(params.id); //dynamic id
+        //OR return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: false,
+  };
+}
+
+const CoffeeStoreDetail = (props) => {
   const router = useRouter();
-
-  console.log("router", router);
-  console.log("router-query", router.query);
-
+  console.log("props", props);
+  // console.log("router", router);
+  // console.log("router-query", router.query);
   const queryId = router.query.id;
 
   return (
@@ -15,7 +37,8 @@ const CoffeeStoreDetail = () => {
         <a>Back to Home</a>
       </Link>
       <h1>This is a dynamic route for CoffeeStoreDetail {queryId}.</h1>
-      <h2>The Query ID is {queryId}.</h2>
+      <h2>Id: {props.coffeeStore.id}</h2>
+      <h3>Name: {props.coffeeStore.name}</h3>
     </div>
   );
 };
