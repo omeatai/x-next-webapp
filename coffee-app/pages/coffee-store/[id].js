1823015748840807784945c2f04bf06ef6fc84f9
omeatai <omeatai@gmail.com>
2023-11-01 00:09:32 -0600

    29. Coffee Store App - Update API in getStaticPaths
    29. Coffee Store App - Update API in getStaticPaths

--- coffee-app/pages/coffee-store/[id].js
+++ coffee-app/pages/coffee-store/[id].js
@@ -2,31 +2,31 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
-
 import cls from "classnames";
 
-import coffeeStoresData from "../../data/coffee-stores.json";
-
+// import coffeeStoresData from "../../data/coffee-stores.json";
 import styles from "../../styles/coffee-store.module.css";
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
 export async function getStaticProps({ params }) {
-  console.log("params", params);
+  // console.log("params", params);
+  const coffeeStores = await fetchCoffeeStores();
 
   return {
     props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
-        return coffeeStore.id === parseInt(params.id); //dynamic id
-        //OR return coffeeStore.id.toString() === params.id;
+      coffeeStore: coffeeStores.find((coffeeStore) => {
+        return coffeeStore.fsq_id.toString() === params.id; //dynamic id
       }),
     },
   };
 }
 
-export function getStaticPaths() {
-  const paths = coffeeStoresData.map((coffeeStore) => {
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map((coffeeStore) => {
     return {
       params: {
-        id: coffeeStore.id.toString(),
+        id: coffeeStore.fsq_id.toString(),
       },
     };
   });
@@ -45,7 +45,7 @@ const CoffeeStoreDetail = (props) => {
     return <div>Loading...</div>;
   }
 
-  const { id, address, name, neighbourhood, imgUrl } = props.coffeeStore;
+  const { fsq_id, location, name, imgUrl } = props.coffeeStore;
 
   const handleUpvoteButton = () => {};
 
@@ -65,7 +65,10 @@ const CoffeeStoreDetail = (props) => {
             <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
-            src={imgUrl}
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
             width={600}
             height={360}
             className={styles.storeImg}
@@ -74,14 +77,20 @@ const CoffeeStoreDetail = (props) => {
         </div>
 
         <div className={cls("glass", styles.col2)}>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/places.svg" width="24" height="24" />
-            <p className={styles.text}>{address}</p>
-          </div>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/nearMe.svg" width="24" height="24" />
-            <p className={styles.text}>{neighbourhood}</p>
-          </div>
+          {location.address && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/places.svg" width="24" height="24" />
+              <p className={styles.text}>{location.address}</p>
+            </div>
+          )}
+
+          {location.locality && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+              <p className={styles.text}>{location.locality}</p>
+            </div>
+          )}
+
           <div className={styles.iconWrapper}>
             <Image src="/static/icons/star.svg" width="24" height="24" />
             <p className={styles.text}>1</p>
@@ -97,3 +106,17 @@ const CoffeeStoreDetail = (props) => {
 };
 
 export default CoffeeStoreDetail;
+
+// {
+//   fsq_id: '59f784dd28122f14f9d5d63d',
+//   categories: [Array],
+//   chains: [],
+//   closed_bucket: 'VeryLikelyOpen',
+//   distance: 870,
+//   geocodes: [Object],
+//   link: '/v3/places/59f784dd28122f14f9d5d63d',
+//   location: [Object],
+//   name: 'HotBlack Coffee',
+//   related_places: {},
+//   timezone: 'America/Toronto'
+// }
