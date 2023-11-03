834a41fb081087e073911565ebb3db84896b7ee7
omeatai <omeatai@gmail.com>
2023-11-03 16:49:42 -0600

    35. Coffee Store App - Handle Error of Fallback True on Dynamic Route
    35. Coffee Store App - Handle Error of Fallback True on Dynamic Route

--- coffee-app/pages/coffee-store/[id].js
+++ coffee-app/pages/coffee-store/[id].js
@@ -11,12 +11,13 @@ import { fetchCoffeeStores } from "../../lib/coffee-stores";
 export async function getStaticProps({ params }) {
   // console.log("params", params);
   const coffeeStores = await fetchCoffeeStores();
+  const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id; //dynamic id
+  });
 
   return {
     props: {
-      coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.id.toString() === params.id; //dynamic id
-      }),
+      coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
     },
   };
 }
